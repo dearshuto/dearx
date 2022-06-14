fab8fa8c3b4aa50183a96e9d2371f520a31b9766
Shuto Shikama <@gmail.com>
2022-06-14 23:01:53 +0900

    Presentation クレートを追加

--- dearx_editor/src/main.rs
+++ dearx_editor/src/main.rs
@@ -1,3 +1,4 @@
+use dearx_presentation::{DesignView, IDesignViewViewModel};
 use egui_winit_platform::{Platform, PlatformDescriptor};
 use epi::egui::FontDefinitions;
 use gfx_egui::RenderPass;
@@ -7,7 +8,6 @@ use sjgfx::{
     TSwapChainBuilder,
 };
 use sjgfx_interface::{ICommandBuffer, IQueue, ISwapChain, TextureArrayRange};
-use winit::event_loop::EventLoop;
 
 struct ExampleRepaintSignal;
 
@@ -17,17 +17,60 @@ impl epi::backend::RepaintSignal for ExampleRepaintSignal {
     }
 }
 
+struct ViewModel<TApi: sjgfx::api::IApi> {
+    _marker: std::marker::PhantomData<TApi>,
+}
+
+impl<TApi: sjgfx::api::IApi> ViewModel<TApi> {
+    pub fn new() -> Self {
+        Self {
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<TApi: sjgfx::api::IApi> IDesignViewViewModel<TApi> for ViewModel<TApi> {
+    fn on_render(&mut self, queue: &mut TApi::Queue) {
+        queue.flush();
+    }
+
+    fn get_camera_position(&self) -> (f32, f32, f32) {
+        todo!()
+    }
+
+    fn get_camera_look_at(&self) -> (f32, f32, f32) {
+        todo!()
+    }
+
+    fn get_camera_up(&self) -> (f32, f32, f32) {
+        (0.0, 1.0, 0.0)
+    }
+}
+
 #[tokio::main]
 async fn main() {
     run::<sjgfx::api::Wgpu>();
 }
 
 fn run<TApi: IApi>() {
-    let event_loop = EventLoop::new();
-    let mut display = sjvi::create_display(event_loop);
+    let mut instance = sjvi::Instance::new();
+    let id = instance.create_display_with_size(2560, 1920);
 
-    let mut device =
-        TDeviceBuilder::<TApi>::new().build_with_surface(&display.window, &display.event_loop);
+    let (mut device, mut platform) = {
+        let display = instance.try_get_display(id).unwrap();
+        let device = TDeviceBuilder::<TApi>::new()
+            .build_with_surface(&display.window, instance.get_event_loop());
+
+        let platform = Platform::new(PlatformDescriptor {
+            physical_width: 1280,
+            physical_height: 960,
+            scale_factor: display.window.scale_factor(),
+            font_definitions: FontDefinitions::default(),
+            style: Default::default(),
+        });
+
+        (device, platform)
+    };
     let mut queue = TQueueBuilder::<TApi>::new().build(&device);
     let mut swap_chain = TSwapChainBuilder::<TApi>::new()
         .with_width(2560)
@@ -35,20 +78,17 @@ fn run<TApi: IApi>() {
         .build(&mut device);
     let mut command_buffer = TCommandBufferBuilder::<TApi>::new().build(&device);
     let mut semaphore = TSemaphoreBuilder::<TApi>::new().build(&device);
-    let mut platform = Platform::new(PlatformDescriptor {
-        physical_width: 1280,
-        physical_height: 960,
-        scale_factor: display.window.scale_factor(),
-        font_definitions: FontDefinitions::default(),
-        style: Default::default(),
-    });
 
     let _render_pass = RenderPass::<TApi>::new(&device);
 
     let _workspace = Workspace::<i32>::new();
 
-    while !display.should_close() {
-        display.update(|| {
+    let mut design_view = DesignView::<TApi, ViewModel<TApi>>::new(&device, ViewModel::new());
+
+    while instance.try_update() {
+        let display = instance.try_get_display(id).unwrap();
+
+        if display.is_redraw_requested() {
             let mut next_scan_buffer_view =
                 swap_chain.acquire_next_scan_buffer_view(Some(&mut semaphore), None);
 
@@ -68,10 +108,15 @@ fn run<TApi: IApi>() {
             // let context = platform.context();
             // platform.end_frame(window);
 
+            // DesignView
+            design_view.process_frame(&mut queue);
+
             queue.execute(&command_buffer);
             queue.present(&mut swap_chain);
             queue.sync();
             queue.flush();
-        });
+        }
+
+        display.listen(&mut swap_chain);
     }
 }
