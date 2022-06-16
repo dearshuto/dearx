c957785c2d019e84b5cb94096f851716055cfeb3
Shuto Shikama <@gmail.com>
2022-06-16 23:11:18 +0900

    dearx_gfx の実装
    決めうちの暫定実装

--- dearx_gfx/resources/shaders/basic.vs
+++ dearx_gfx/resources/shaders/basic.vs
@@ -6,14 +6,18 @@ layout(location = 1) out vec3 v_Normal;
 layout(location = 0) in vec3 i_Position;
 layout(location = 1) in vec3 i_Normal;
 
-layout(binding = 0) uniform View {
+layout(binding = 0) uniform ModelData {
   mat4 u_ModelMatrix;
+};
+
+layout(binding = 1) uniform ViewData {
   mat4 u_ProjectionViewMatrix;
 };
 
 void main() {
   vec4 worldPosition = u_ModelMatrix * vec4(i_Position, 1.0);
   vec4 position = u_ProjectionViewMatrix * worldPosition;
+  gl_Position = position;
 
   v_WorldPosition = worldPosition.xyz;
   v_Normal = i_Normal;
--- dearx_gfx/src/component/geometry_component.rs
+++ dearx_gfx/src/component/geometry_component.rs
@@ -1,11 +1,91 @@
+extern crate nalgebra_glm as glm;
+
+use sjgfx::{api::IApi, TBufferBuilder};
+use sjgfx_interface::IBuffer;
+
 use super::TransformComponent;
 
-pub struct GeometryComponent;
+#[repr(C)]
+struct ViewData {
+    model_matrix: glm::Mat4x4,
+}
 
-impl GeometryComponent {
-    pub fn update(&mut self, _transform: &TransformComponent) {}
+pub struct GeometryComponent<TApi: IApi> {
+    vertex_buffer: TApi::Buffer,
+    index_buffer: TApi::Buffer,
+    constant_buffer: TApi::Buffer,
+    transform_index: u32,
+    index_count: i32,
+}
+
+impl<TApi: IApi> GeometryComponent<TApi> {
+    pub fn new(device: &TApi::Device, transform_index: u32) -> Self {
+        // 頂点バッファ
+        let vertex_data = [
+            -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            1.0,
+        ];
+        let vertex_buffer = TBufferBuilder::<TApi>::new()
+            .enable_vertex_buffer()
+            .with_size(std::mem::size_of::<f32>() * vertex_data.len())
+            .build(device);
+        vertex_buffer.map_as_slice_mut(|data: &mut [f32]| {
+            data.clone_from_slice(&vertex_data);
+        });
+
+        // インデックスバッファ
+        let index_data = [0, 1, 2];
+        let index_buffer = TBufferBuilder::<TApi>::new()
+            .enable_index_buffer()
+            .with_size(std::mem::size_of::<u32>() * index_data.len())
+            .build(device);
+        index_buffer.map_as_slice_mut(|data: &mut [u32]| {
+            data.clone_from_slice(&index_data);
+        });
+
+        let constant_buffer = TBufferBuilder::<TApi>::new()
+            .enable_constant_buffer()
+            .with_size(std::mem::size_of::<ViewData>())
+            .build(device);
+
+        Self {
+            vertex_buffer,
+            index_buffer,
+            constant_buffer,
+            transform_index,
+            index_count: index_data.len() as i32,
+        }
+    }
+
+    pub fn update(&mut self, transform: &mut TransformComponent) {
+        self.constant_buffer.map_mut(|data: &mut ViewData| {
+            let translation = glm::vec3(
+                transform.translation.x,
+                transform.translation.y,
+                transform.translation.z,
+            );
+            let translation_matrix = glm::translation(&translation);
+            data.model_matrix = translation_matrix;
+        });
+    }
 
     pub fn get_transform_index(&self) -> u32 {
-        todo!()
+        self.transform_index
+    }
+
+    pub fn get_vertex_buffer(&self) -> &TApi::Buffer {
+        &self.vertex_buffer
+    }
+
+    pub fn get_index_buffer(&self) -> &TApi::Buffer {
+        &self.index_buffer
+    }
+
+    pub fn get_index_count(&self) -> i32 {
+        self.index_count
+    }
+
+    pub fn get_model_data(&self) -> &TApi::Buffer {
+        &self.constant_buffer
     }
 }
