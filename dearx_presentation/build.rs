50dd54d149a9009a25df8495bf494620824d5dd2
Shuto Shikama <@gmail.com>
2022-08-09 02:24:20 +0900

    [WIP] フロントエンドを html に置き換え

--- dearx_presentation/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let mut compiler = sjgfx_util::ShaderCompiler::new();
-
-    // グリッド
-    compiler.build_graphics_shader(&"resources/shaders/grid.vs", &"resources/shaders/grid.fs");
-}
--- dearx_presentation/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod design_view;
-mod property_window;
-
-pub use design_view::{DesignView, IDesignViewViewModel};
-pub use property_window::{IPropertyWindowViewModel, PropertyWindow};
