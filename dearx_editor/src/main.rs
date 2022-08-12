5c08630e6e37a7892afd53b58dccadebec95d5a5
Shuto Shikama <>
2022-08-13 01:40:12 +0900

    Model から View にイベントを飛ばす処理を追加

--- dearx_editor/src/main.rs
+++ dearx_editor/src/main.rs
@@ -3,6 +3,8 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 #[tauri::command]
 fn my_custom_command() {
     println!("I was invoked from JS!");
@@ -15,6 +17,16 @@ fn on_value_changed(value: f64) {
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+        let app_handle = app.app_handle();
+        std::thread::spawn(move || loop {
+            app_handle
+                .emit_all("back-to-front", "ping frontend".to_string())
+                .unwrap();
+            std::thread::sleep(std::time::Duration::from_secs(1))
+        });
+        Ok(())
+    })
     .invoke_handler(tauri::generate_handler![my_custom_command, on_value_changed])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
