1610ec8ed96020aca0327ddf2ee48fc54aa6609e
Shuto Shikama <>
2022-08-13 19:08:20 +0900

    JavaScript のリファクタリング
    HTML 内のヘッダで JavaScript をロードできるように遅延ロードの設定を追加

--- dearx_presentation/index.js
+++ dearx_presentation/index.js
@@ -1,3 +1,9 @@
+// 初期化処理
+document.addEventListener('DOMContentLoaded', function() {
+    let input = document.getElementById('input_x');
+    input.value = 0;
+}, {'once': true});
+
 function func() {
     window.__TAURI__
       .invoke("my_custom_command")
@@ -7,19 +13,17 @@ function func() {
       })
   }
 
-  function on_value_changed() {
+function on_value_changed() {
+    let input = document.getElementById('input_x');
+  window.__TAURI__
+    .invoke("on_value_changed", { value: parseFloat(input.value) })
+    .then((response) => {
+    })
+    .catch((error) => {
+    })
+}
 
-    window.__TAURI__
-      .invoke("on_value_changed", { value: parseFloat(input.value) })
-      .then((response) => {
-      })
-      .catch((error) => {
-      })
-  }
-
-  let input = document.getElementById('input_x');
-  input.value = 0;
-
-  window.__TAURI__.event.listen('back-to-front', event => {
-    console.log(`back-to-front ${event.payload} ${new Date()}`)
-  });
+// バックエンドからのメッセージのハンドリング
+window.__TAURI__.event.listen('back-to-front', event => {
+  console.log(`back-to-front ${event.payload} ${new Date()}`)
+});
