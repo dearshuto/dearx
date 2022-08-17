12fb60443dc1b16460b28312fa5bce564f02a292
Shuto Shikama <@gmail.com>
2022-08-17 23:02:05 +0900

    モデル層の実装
    Workspace にドキュメントを追加
    ViewModel に処理を移動

--- dearx_editor/src/property_window_view_model.rs
+++ dearx_editor/src/property_window_view_model.rs
@@ -1,14 +1,25 @@
 use std::sync::{Arc, Mutex};
 
 use dearx_edit_model::DearxProject;
-use dearx_workspace::Workspace;
+use dearx_workspace::{DocumentId, Workspace};
 
 pub struct PropertyWindowViewModel {}
 
 impl PropertyWindowViewModel {
-    pub fn new(
-        _workspace: Arc<Mutex<Workspace<DearxProject>>>,
-    ) -> Self {
+    pub fn new(id: &DocumentId, workspace: Arc<Mutex<Workspace<DearxProject>>>) -> Self {
+        let id = id.clone();
+        workspace
+            .lock()
+            .unwrap()
+            .observe_project()
+            .lock()
+            .unwrap()
+            .subscribe(move |project| {
+                if let Some(document) = project.documents.get(&id) {
+                    println!("Value: {}", document.content.value);
+                }
+            });
+
         Self {}
     }
 }
