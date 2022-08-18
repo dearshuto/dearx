2ee97d54d97ef2820c38f6f51fea2bad9f280529
Shuto Shikama <>
2022-08-18 23:43:02 +0900

    選択状態の変更を　View から Model に通知

---
+++ dearx_editor/src/object_tree_view_model.rs
@@ -0,0 +1,20 @@
+use std::sync::{Arc, Mutex};
+
+use dearx_edit_model::DearxProject;
+use dearx_workspace::{DocumentId, Workspace};
+
+use tauri::Manager;
+
+pub struct ObjectTreeViewModel;
+
+impl ObjectTreeViewModel {
+    pub fn new(_id: &DocumentId, _workspace: Arc<Mutex<Workspace<DearxProject>>>) -> Self {
+        Self {}
+    }
+
+    pub fn listen(&self, app: &tauri::App) {
+        app.listen_global("selection_changed", |event| {
+            println!("selection_changed: {:?}", event.payload());
+        });
+    }
+}
