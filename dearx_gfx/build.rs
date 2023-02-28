c824e5a10fee60767a8e03ca4eeafe04ee92f427
Shuto Shikama <@gmail.com>
2023-03-01 00:22:52 +0900

    gfx で presentation 構築
    wasm のビルドエラーを修正

--- dearx_gfx/build.rs
+++ dearx_gfx/build.rs
@@ -1,11 +1,13 @@
+#[cfg(target_arch = "wasm32")]
+fn main() {}
+
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     let mut compiler = sjgfx_util::ShaderCompiler::new();
 
     // ベーシック
-    compiler.build_graphics_shader(
-        &"resources/shaders/basic.vs",
-        &"resources/shaders/basic.fs",
-    );
+    compiler.build_graphics_shader(&"resources/shaders/basic.vs", &"resources/shaders/basic.fs");
+
     // gRPC
     tonic_build::configure()
         .build_server(true)
