ddc4ea29028230add4412f5ecd6aa57463452573
Shuto Shikama <@gmail.com>
2023-03-21 15:20:56 +0900

    通信プロトコル拡張

--- crates/dearx_gfx/dearx_viewer/build.rs
+++ crates/dearx_gfx/dearx_viewer/build.rs
@@ -1,7 +1,19 @@
 fn main() {
     tonic_build::configure()
         .build_server(false)
-        .build_client(false)
+        .build_client(true)
+        .type_attribute(
+            "dearx_viewer.GetRequest",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            "dearx_viewer.GetMeshRequest",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            "dearx_viewer.GetSceneInfoRequest",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
         .compile(
             &["proto/dearx_viewer.proto"],
             &Vec::<std::path::PathBuf>::new(),
--- crates/dearx_gfx/dearx_viewer/src/http/client.rs
+++ crates/dearx_gfx/dearx_viewer/src/http/client.rs
@@ -1,4 +1,4 @@
-use crate::proto::{Color, Mesh};
+use crate::proto::{Color, GetMeshRequest, GetRequest, Mesh};
 use prost::Message;
 use std::result::Result;
 
@@ -20,24 +20,28 @@ impl Client {
     }
 
     pub async fn fetch_color(&mut self) -> Result<Color, ()> {
+        let request = GetRequest {
+            ..Default::default()
+        };
         let response = self
             .client
             .get("http://localhost:3000/color")
+            .query(&request)
             .send()
             .await
             .unwrap();
         let bytes = response.bytes().await.unwrap();
+        println!("{}: {:?}", bytes.len(), bytes);
         let color = Color::decode(bytes).unwrap();
         Ok(color)
     }
 
     pub async fn fetch_mesh(&mut self) -> Result<Mesh, ()> {
-        let response = self
-            .client
-            .get("http://localhost:3000/mesh")
-            .send()
-            .await
-            .unwrap();
+        let request = GetMeshRequest {
+            ..Default::default()
+        };
+        let url = "http://localhost:3000/mesh";
+        let response = self.client.get(url).query(&request).send().await.unwrap();
         let bytes = response.bytes().await.unwrap();
         let mesh = Mesh::decode(bytes).unwrap();
         Ok(mesh)
