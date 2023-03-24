bdd2c96aa80fca46062df674e908ec41ec7e4e0d
Shuto Shikama <@gmail.com>
2023-03-25 01:04:40 +0900

    ファイル構造整理

---
+++ editor/3_dearx_edit_model/src/dearx_project.rs
@@ -0,0 +1,29 @@
+use dearx_macro::Immutable;
+use dearx_workspace::DocumentId;
+use im::HashMap;
+use std::sync::Arc;
+
+use crate::{GameObject, GameObjectId};
+
+#[derive(Default, Immutable)]
+pub struct DearxProject {
+    pub game_object: Arc<HashMap<GameObjectId, GameObject>>,
+    pub selections: Arc<im::Vector<DocumentId>>,
+
+    // 動作確認用のデータ。将来的に消す。
+    pub color: [f32; 3],
+    pub vertives: Vec<f32>,
+    pub indices: Vec<u32>,
+}
+
+impl DearxProject {
+    pub fn new() -> Self {
+        Self {
+            game_object: Default::default(),
+            selections: Default::default(),
+            color: Default::default(),
+            vertives: Default::default(),
+            indices: Default::default(),
+        }
+    }
+}
