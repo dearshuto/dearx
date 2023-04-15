14c83b4f926f9f9233cf6f8a73784df654e324c6
Shuto Shikama <@gmail.com>
2023-04-16 00:16:37 +0900

    不要なクレートを削除

--- crates/dearx_gfx/build.rs
+++ crates/dearx_gfx/build.rs
@@ -3,11 +3,4 @@ fn main() {
 
     // ベーシック
     compiler.build_graphics_shader(&"resources/shaders/basic.vs", &"resources/shaders/basic.fs");
-
-    // gRPC
-    tonic_build::configure()
-        .build_server(false)
-        .build_client(false)
-        .compile(&["proto/dearx.proto"], &Vec::<std::path::PathBuf>::new())
-        .unwrap();
 }
