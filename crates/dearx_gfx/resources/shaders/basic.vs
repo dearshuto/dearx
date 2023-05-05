f7408f143785e837430fad259260479fc4b022e8
Shuto Shikama <@gmail.com>
2023-05-06 00:34:18 +0900

    複数シェーダのセットアップ機能を実装

--- crates/dearx_gfx/resources/shaders/basic.vs
+++ crates/dearx_gfx/resources/shaders/basic.vs
@@ -7,16 +7,24 @@ layout(location = 0) in vec3 i_Position;
 layout(location = 1) in vec3 i_Normal;
 
 layout(binding = 0) uniform ModelData {
-  mat4 u_ModelMatrix;
+  vec4 u_ModelMatrix[4];
 };
 
 layout(binding = 1) uniform ViewData {
-  mat4 u_ProjectionViewMatrix;
+  vec4 u_ProjectionViewMatrix[4];
 };
 
 void main() {
-  vec4 worldPosition = u_ModelMatrix * vec4(i_Position, 1.0);
-  vec4 position = u_ProjectionViewMatrix * worldPosition;
+  vec4 worldPosition = vec4(
+    dot(u_ModelMatrix[0], vec4(i_Position, 1.0)),
+    dot(u_ModelMatrix[1], vec4(i_Position, 1.0)),
+    dot(u_ModelMatrix[2], vec4(i_Position, 1.0)),
+    dot(u_ModelMatrix[3], vec4(i_Position, 1.0)));
+  vec4 position = vec4(
+    dot(u_ProjectionViewMatrix[0], worldPosition),
+    dot(u_ProjectionViewMatrix[1], worldPosition),
+    dot(u_ProjectionViewMatrix[2], worldPosition),
+    dot(u_ProjectionViewMatrix[3], worldPosition));
   gl_Position = position;
 
   v_WorldPosition = worldPosition.xyz;
--- crates/dearx_gfx/src/serializer/deserializer.rs
+++ crates/dearx_gfx/src/serializer/deserializer.rs
@@ -27,6 +27,7 @@ pub struct CreateBufferDescriptor<'a> {
 pub struct CreateRenderPipelineDescriptor<'a> {
     pub vertex_shader: &'a [u8],
     pub pixel_shader: Option<&'a [u8]>,
+    pub vertex_buffer_offsets: Vec<isize>,
     pub texture_format: Option<sjgfx_interface::ImageFormat>,
 }
 
@@ -42,19 +43,21 @@ pub fn deserialize<TFactory: IFactory>(
     // let mut stream_reader = usd_rs::StreamReader::new(data);
     // let _reader = usd_rs::AsciiReader::new(&mut stream_reader);
 
-    // シェーダー
-    let vertex_shader_source = include_str!("../../resources/shaders/triangle.vs");
-    let pixel_shader_source = include_str!("../../resources/shaders/triangle.fs");
-    let mut compiler = sjgfx_util::ShaderCompiler::new();
-    let vertex_shader_binary =
-        compiler.create_binary(vertex_shader_source, sjgfx_util::ShaderStage::Vertex);
-    let pixel_shader_binary =
-        compiler.create_binary(pixel_shader_source, sjgfx_util::ShaderStage::Pixel);
-    let pipeline = factory.create_render_pipeline(&CreateRenderPipelineDescriptor {
-        vertex_shader: &vertex_shader_binary,
-        pixel_shader: Some(&pixel_shader_binary),
-        texture_format: None,
-    });
+    // 2D 描画シェーダー
+    let (pipeline, vertex_shader_binary, pixel_shader_binary) = create_render_pipeline(
+        factory,
+        include_str!("../../resources/shaders/triangle.vs"),
+        include_str!("../../resources/shaders/triangle.fs"),
+        vec![(std::mem::size_of::<f32>() * 3) as isize],
+    );
+
+    // 3D 描画シェーダー
+    let (pipeline_3d, vertex_shader_binary_3d, pixel_shader_binary_3d) = create_render_pipeline(
+        factory,
+        include_str!("../../resources/shaders/basic.vs"),
+        include_str!("../../resources/shaders/basic.fs"),
+        vec![(std::mem::size_of::<f32>() * 6) as isize],
+    );
 
     let vertex_buffer_data0 =
         bytemuck::cast_slice(&[-0.40f32, -0.25, 0.0, 0.10, -0.25, 0.0, -0.15, 0.25, 0.0]);
@@ -96,13 +99,40 @@ pub fn deserialize<TFactory: IFactory>(
         vertex_shader: &vertex_shader_binary,
         pixel_shader: &pixel_shader_binary,
     });
+    let bind_group_3d = factory.create_descriptor_pool(&CreateDescriptorPoolDescriptor {
+        vertex_shader: &vertex_shader_binary_3d,
+        pixel_shader: &pixel_shader_binary_3d,
+    });
 
     SceneObject {
         vertex_buffers: vec![vertex_buffer0, vertex_buffer1],
         constant_buffers: vec![model_data_buffer, view_buffer],
-        pipelines: vec![pipeline],
-        descriptor_pool: vec![bind_group],
+        pipelines: vec![pipeline, pipeline_3d],
+        descriptor_pool: vec![bind_group, bind_group_3d],
     }
+}
+
+fn create_render_pipeline<TFactory>(
+    factory: &mut TFactory,
+    vertex_shader_source: &str,
+    pixel_shader_source: &str,
+    vertex_buffer_offsets: Vec<isize>,
+) -> (TFactory::TRenderPipeline, Vec<u8>, Vec<u8>)
+where
+    TFactory: IFactory,
+{
+    let mut compiler = sjgfx_util::ShaderCompiler::new();
+    let vertex_shader_binary =
+        compiler.create_binary(vertex_shader_source, sjgfx_util::ShaderStage::Vertex);
+    let pixel_shader_binary =
+        compiler.create_binary(pixel_shader_source, sjgfx_util::ShaderStage::Pixel);
+    let pipeline = factory.create_render_pipeline(&CreateRenderPipelineDescriptor {
+        vertex_shader: &vertex_shader_binary,
+        pixel_shader: Some(&pixel_shader_binary),
+        vertex_buffer_offsets,
+        texture_format: None,
+    });
+    (pipeline, vertex_shader_binary, pixel_shader_binary)
 }
 
 unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
